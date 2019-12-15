9d29b1cf7c5ef82f1910877d6d41703173eb4311
Sandesh <stimilsina@unm.edu>
2019-12-15 09:52:15 -0700

    added new code for running in carc

--- simulation.py
+++ simulation.py
@@ -10,12 +10,27 @@ import matplotlib.pyplot as plt
 from IPR import ipr
 allSDList =[]
 allIPR =[]
-for i in range(0,1000):
-    #allSDList.extend(sd())
-    allIPR.extend(ipr())
 
+def zerolistmaker(n):
+    listofzeros = [0] * n
+    return listofzeros
+#for i in range(0,1000):
+#    #allSDList.extend(sd())
+#    allIPR =ipr()
+#    f = open('iprList.txt','a')
+#    np.savetxt(f, allIPR ,fmt='%1.3f',delimiter =',')
+#    f.close()
+Partition = np.linspace(0,200,2000)    
+datas = zerolistmaker(len(Partition))   
+for i in range(0,200):
+    allIPR = ipr()
+    sortedList = np.searchsorted(Partition,allIPR)
+    frequency = np.bincount(sortedList)[1:]
+    for i in range(0,len(frequency)):
+        datas[i] = datas[i] + frequency[i]        
+np.savetxt('binned.txt', datas, delimiter=',',fmt='%1.0f')    
 print("this is size of list ", len(allIPR))
-np.savetxt('iprlist.txt', allIPR, delimiter =',')
+#np.savetxt('iprlist.txt', allIPR, delimiter =',')
 #np.savetxt('sdlist.txt',allSDList, delimiter= ',')
 #plt.hist(allSDList)
 
