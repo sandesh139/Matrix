baa1d6b2aa17b865a77b1ddd52b4e2402e541b3b
Sandesh <stimilsina@unm.edu>
2019-12-20 01:30:31 -0700

    added merge file

--- Sd-200/BinMerge.py
+++ Sd-200/BinMerge.py
@@ -7,9 +7,9 @@ Created on Sun Dec 15 11:15:08 2019
 import numpy as np
 from operator import add
 import matplotlib.pyplot as plt
-binnedData = np.loadtxt("binned.txt", comments="#", delimiter=",", unpack=False)
-binnedData1 = np.loadtxt("binned1.txt", comments="#", delimiter=",", unpack=False)
-binnedData22 = np.loadtxt("binned22.txt", comments="#", delimiter=",", unpack=False)
+binnedData = np.loadtxt("binned1.txt", comments="#", delimiter=",", unpack=False)
+binnedData1 = np.loadtxt("binned2.txt", comments="#", delimiter=",", unpack=False)
+binnedData22 = np.loadtxt("binned3.txt", comments="#", delimiter=",", unpack=False)
 mergeData = list(map(add,binnedData,binnedData1))
 mergeData = list(map(add,mergeData,binnedData22))
 np.savetxt('bin_merge.txt', mergeData, delimiter=',',fmt='%1.0f')
@@ -17,4 +17,4 @@ out = np.divide(mergeData, 10000000)
 sumBin = sum(mergeData)
 partitioned = out[0:250]
 plt.plot(partitioned)
-plt.savefig('plot1.png')
+plt.savefig('plotMerge.png')
